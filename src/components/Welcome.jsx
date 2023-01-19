c9818f10274aa736f797c806c99a784da4df65ec
Marlon Couto <marloncouto.dev@gmail.com>
2023-01-19 11:55:31 -0300

    Adiciona o componente Welcome

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import quiz from '../assets/img/quiz.svg';
+
+const Welcome = () => {
+  return (
+    <div id="welcome">
+      <h2>Seja bem-vindo</h2>
+      <p>Clique no botão abaixo para começar:</p>
+      <button>Iniciar</button>
+      <img
+        src={quiz}
+        alt="Início do Quiz"
+      />
+    </div>
+  );
+};
+
+export default Welcome;
