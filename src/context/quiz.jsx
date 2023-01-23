554e23e82d6ab8fce9e2a5bb6972e6b44fe72578
Marlon Couto <marloncouto.dev@gmail.com>
2023-01-23 12:35:42 -0300

    Feat: ir para a pergunta seguinte do quiz

--- src/context/quiz.jsx
+++ src/context/quiz.jsx
@@ -19,10 +19,16 @@ const quizReducer = (state, action) => {
 
     case 'REORDER_QUESTIONS':
       const reorderedQuestions = questions.sort(() => Math.random() - 0.5);
-
       return {
         ...state,
         questions: reorderedQuestions,
+      };
+
+    case 'CHANGE_QUESTION':
+      const nextQuestion = state.currentQuestion + 1;
+      return {
+        ...state,
+        currentQuestion: nextQuestion,
       };
 
     default:
